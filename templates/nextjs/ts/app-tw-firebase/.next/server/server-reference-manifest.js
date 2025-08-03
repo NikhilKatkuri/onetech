e8bc2ea16b238843cbb5896d7b0b625fbb44ba45
Nikhil <knikhil07k@gmail.com>
2025-08-03 17:49:17 +0530

    feat(add): new feature nextjs+firebase in javascript

--- templates/nextjs/ts/app-tw-firebase/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"bVif1olHXENRqrDUXMXco8I2NrbYtrzbDXH9BDlYrsI=\"\n}"
--- templates/nextjs/ts/app-tw-firebase/.next/static/chunks/src_app_layout_tsx_68b267f5._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_b805903d.css",
-  "static/chunks/node_modules_1c5e3de6._.js",
-  "static/chunks/src_a01a3cfa._.js"
-],
-    source: "dynamic"
-});
